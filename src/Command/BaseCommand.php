553aa867d6239ea4472daf5550f166851417c103
Lucjan Wilczewski <lucjan@desmart.com>
2016-02-19 12:04:12 +0100

    Fixes issue with namespace prefixed with root

---
+++ src/Command/BaseCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DeSmart\DeMaker\Core\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BaseCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setDefinition(
+            new InputDefinition([
+                new InputArgument('fqn', InputArgument::REQUIRED, 'FQN of the class to be generated'),
+                new InputArgument('testfqn', InputArgument::REQUIRED, 'FQN of the test for the class to be generated'),
+            ])
+        );
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $input->setArgument('fqn', $this->sanitizeFQN($input->getArgument('fqn')));
+        $input->setArgument('testfqn', $this->sanitizeFQN($input->getArgument('testfqn')));
+    }
+
+    /**
+     * @param string $fqn
+     * @return string
+     */
+    protected function sanitizeFQN($fqn)
+    {
+        return ltrim($fqn, '\\');
+    }
+}
